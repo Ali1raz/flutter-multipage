ec34647a858709317a56a64de14ca0f9c91217dc
Ali Raza <alirazakhalid1129@gmail.com>
2025-04-01 07:37:17 +0500

    refactor base_screen

--- lib/pages/base_screen.dart
+++ lib/pages/base_screen.dart
@@ -5,70 +5,51 @@ class BaseScreen extends StatelessWidget {
   final Widget child;
   final String title;
 
-  const BaseScreen({
-    super.key,
-    required this.child,
-    required this.title
-});
+  const BaseScreen({super.key, required this.child, required this.title});
 
   @override
   Widget build(BuildContext context) {
     String currentRoute = ModalRoute.of(context)?.settings.name ?? '/';
 
     return Scaffold(
-      appBar: AppBar(title: Text(title, style: TextStyle(color: Colors.white),),backgroundColor: Colors.blueAccent,),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue),
-              child: Text(
-                "Drawer Example",
-                style: TextStyle(color: Colors.white, fontSize: 18),
-              ),
-            ),
-            DrawerTile(
-              icon: Icons.home,
-              title: "Home",
-              isSelected: currentRoute == '/',
-              onTap: () {
-                if (currentRoute != '/') {
-                  Navigator.pushNamed(context, '/');
-                }
-              },
-            ),
-            DrawerTile(
-              icon: Icons.card_giftcard,
-              title: "Flash cards",
-              isSelected: currentRoute == '/flashcard',
-              onTap: () {
-                if (currentRoute != '/flashcard') {
-                  Navigator.pushNamed(
-                    context,
-                    '/flashcard',
-                  );
-                }
-              },
-            ),
-            DrawerTile(
-              icon: Icons.card_giftcard,
-              title: "Settings",
-              isSelected: currentRoute == '/settings',
-              onTap: () {
-                if (currentRoute != '/settings') {
-                  Navigator.pushNamed(
-                    context,
-                    '/settings',
-                  );
-                }
-              },
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text(title, style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blueAccent,
       ),
-
+      drawer: _buildDrawer(context),
       body: child,
     );
+  }
+
+  Widget _buildDrawer(BuildContext context) {
+    String currentRoute = ModalRoute.of(context)?.settings.name ?? '/';
+
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(color: Colors.blue),
+            child: Text(
+              "Drawer Example",
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+          ),
+          _buildDrawerItem(context, Icons.home_outlined, "Home", '/', currentRoute),
+          _buildDrawerItem(context, Icons.card_giftcard_outlined, "Flashcard", '/flashcard', currentRoute),
+          _buildDrawerItem(context, Icons.settings_outlined, "Settings", '/settings', currentRoute),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDrawerItem(BuildContext context, IconData icon, String title, String route, String currentRoute) {
+    return DrawerTile(icon: icon, title: title, isSelected: currentRoute == route, onTap:() { _navigateToRoute(context, route, currentRoute);});
+  }
+
+  void _navigateToRoute (BuildContext context, String route, String currentRoute) {
+    if (currentRoute != route) {
+      Navigator.pushNamed(context, route);
+    }
   }
 }
